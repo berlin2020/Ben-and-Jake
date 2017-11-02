0fa8ceeb5caf337eb81fcf0a2c6a9e4766f6ef85
Ben <coolmisterbb22@aol.com>
2017-11-02 13:36:17 -0700

    Stuff

--- lightswitchPrototype.c
+++ lightswitchPrototype.c
@@ -5,30 +5,29 @@
 task main()
 {
 	int clapCount = 0;
+	int moveMode = 0;
 
 	while (true)
 	{
 		displayTextLine(4, "Sound %d", SensorValue(sound));
 
-		if(SensorValue(sound) >= 50)
-		{
-			clapCount = clapCount + 1;
-			if(clapCount >= 2)
-			{
-				clapCount = 1;
-			}
-		}
-
-		if(clapCount == 1 && SensorValue(sound) <= 40)
+		if(moveMode == 0 && SensorValue(sound) >= 50)
 		{
 			clapCount = clapCount + 1;
 		}
 
-		if(clapCount == 2)
+		if(clapCount == 1)
 		{
-			motor(light) = 30;
+			moveMode = 1;
+			clapCount = 0;
+		}
+
+		if(moveMode == 1 && SensorValue(sound) <= 40)
+		{
+			motor(light) = 20;
 			wait1Msec(300);
 			clapCount = 0;
+			moveMode = 0;
 		}
 
 	}
